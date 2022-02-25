6d5d0145449eda6c0dac0afbb7998f68046c181a
Melona-BS <han9119693@naver.com>
2022-02-25 15:06:31 +0900

    Readme update

--- Readme.md
+++ Readme.md
@@ -1,5 +1,15 @@
 본 프로젝트는 DMTlabs의 번역 의뢰 웹 프로젝트입니다.
 
+Version
+
+>    Node.js        |   v14.16.1
+>    npm            |   v6.14.12
+>    Vue            |   v2.6.14
+>    Vuetify        |   v2.6.2
+>    Nuxt           |   v2.15.8
+>>      Nuxt/axios      |   v5.13.6
+>>      Nuxt/vuetify    |   v1.12.3
+
 Build
 
 >    npm run build
--- nuxt.config.js
+++ nuxt.config.js
@@ -4,6 +4,37 @@ module.exports = {
         link: [{
             rel: 'icon', type: 'image/x-icon', href: '/favicon.ico'
         }],
+        meta: [{
+            charset: 'utf-8'
+        }, {
+            name: 'viewport',
+            content: 'width=device-width,initial-scale=1.0,minimum-scale=1.0,maximum-scale=1.0,user-scalable=yes,viewport-fit=cover'
+        }, {
+            'http-equiv': 'X-UA-Compatible', content: 'IE=edge',
+        }, {
+            name: 'description',
+            content: 'DMT Translate Web'
+        }, {
+            hid: 'maintitle',
+            name: 'og:title',
+            content: 'DMT',
+        }, {
+            hid: 'maindesc',
+            name: 'og:description',
+            content: 'DMT Translate Web'
+        }, {
+            hid: 'maintype',
+            property: 'og:type',
+            content: 'website',
+        }, /*{
+            hid: 'mainimage',
+            property: 'og:image',
+            content: ''
+        }, {
+            hid: 'mainurl',
+            property: 'og:url',
+            content: ''
+        },*/]
     },
     buildModules: [
         '@nuxtjs/vuetify',
