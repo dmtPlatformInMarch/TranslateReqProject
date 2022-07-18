5fa82f911ad9a693102e4f07c128447a6e01b560
Melona-BS <han9119693@naver.com>
2022-07-18 17:04:36 +0900

    Update : msToString method

--- .nuxt/dist/server/pages/video/filetrack.js
+++ .nuxt/dist/server/pages/video/filetrack.js
@@ -1005,7 +1005,7 @@ if(content.locals) module.exports = content.locals;
 // add CSS to SSR context
 var add = __webpack_require__(6).default
 module.exports.__inject__ = function (context) {
-  add("d5e9c590", content, true, context)
+  add("9b25f63a", content, true, context)
 };
 
 /***/ }),
@@ -1033,9 +1033,9 @@ module.exports.__inject__ = function (context) {
 
 "use strict";
 __webpack_require__.r(__webpack_exports__);
-/* harmony import */ var _node_modules_vuetify_loader_lib_loader_js_ref_4_node_modules_vue_style_loader_index_js_ref_3_oneOf_1_0_node_modules_css_loader_dist_cjs_js_ref_3_oneOf_1_1_node_modules_vue_loader_lib_loaders_stylePostLoader_js_node_modules_postcss_loader_src_index_js_ref_3_oneOf_1_2_node_modules_vue_loader_lib_index_js_vue_loader_options_TrackComponent_vue_vue_type_style_index_0_id_7b653b98_scoped_true_lang_css___WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(283);
-/* harmony import */ var _node_modules_vuetify_loader_lib_loader_js_ref_4_node_modules_vue_style_loader_index_js_ref_3_oneOf_1_0_node_modules_css_loader_dist_cjs_js_ref_3_oneOf_1_1_node_modules_vue_loader_lib_loaders_stylePostLoader_js_node_modules_postcss_loader_src_index_js_ref_3_oneOf_1_2_node_modules_vue_loader_lib_index_js_vue_loader_options_TrackComponent_vue_vue_type_style_index_0_id_7b653b98_scoped_true_lang_css___WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_node_modules_vuetify_loader_lib_loader_js_ref_4_node_modules_vue_style_loader_index_js_ref_3_oneOf_1_0_node_modules_css_loader_dist_cjs_js_ref_3_oneOf_1_1_node_modules_vue_loader_lib_loaders_stylePostLoader_js_node_modules_postcss_loader_src_index_js_ref_3_oneOf_1_2_node_modules_vue_loader_lib_index_js_vue_loader_options_TrackComponent_vue_vue_type_style_index_0_id_7b653b98_scoped_true_lang_css___WEBPACK_IMPORTED_MODULE_0__);
-/* harmony reexport (unknown) */ for(var __WEBPACK_IMPORT_KEY__ in _node_modules_vuetify_loader_lib_loader_js_ref_4_node_modules_vue_style_loader_index_js_ref_3_oneOf_1_0_node_modules_css_loader_dist_cjs_js_ref_3_oneOf_1_1_node_modules_vue_loader_lib_loaders_stylePostLoader_js_node_modules_postcss_loader_src_index_js_ref_3_oneOf_1_2_node_modules_vue_loader_lib_index_js_vue_loader_options_TrackComponent_vue_vue_type_style_index_0_id_7b653b98_scoped_true_lang_css___WEBPACK_IMPORTED_MODULE_0__) if(["default"].indexOf(__WEBPACK_IMPORT_KEY__) < 0) (function(key) { __webpack_require__.d(__webpack_exports__, key, function() { return _node_modules_vuetify_loader_lib_loader_js_ref_4_node_modules_vue_style_loader_index_js_ref_3_oneOf_1_0_node_modules_css_loader_dist_cjs_js_ref_3_oneOf_1_1_node_modules_vue_loader_lib_loaders_stylePostLoader_js_node_modules_postcss_loader_src_index_js_ref_3_oneOf_1_2_node_modules_vue_loader_lib_index_js_vue_loader_options_TrackComponent_vue_vue_type_style_index_0_id_7b653b98_scoped_true_lang_css___WEBPACK_IMPORTED_MODULE_0__[key]; }) }(__WEBPACK_IMPORT_KEY__));
+/* harmony import */ var _node_modules_vuetify_loader_lib_loader_js_ref_4_node_modules_vue_style_loader_index_js_ref_3_oneOf_1_0_node_modules_css_loader_dist_cjs_js_ref_3_oneOf_1_1_node_modules_vue_loader_lib_loaders_stylePostLoader_js_node_modules_postcss_loader_src_index_js_ref_3_oneOf_1_2_node_modules_vue_loader_lib_index_js_vue_loader_options_TrackComponent_vue_vue_type_style_index_0_id_4478e97e_scoped_true_lang_css___WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(283);
+/* harmony import */ var _node_modules_vuetify_loader_lib_loader_js_ref_4_node_modules_vue_style_loader_index_js_ref_3_oneOf_1_0_node_modules_css_loader_dist_cjs_js_ref_3_oneOf_1_1_node_modules_vue_loader_lib_loaders_stylePostLoader_js_node_modules_postcss_loader_src_index_js_ref_3_oneOf_1_2_node_modules_vue_loader_lib_index_js_vue_loader_options_TrackComponent_vue_vue_type_style_index_0_id_4478e97e_scoped_true_lang_css___WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_node_modules_vuetify_loader_lib_loader_js_ref_4_node_modules_vue_style_loader_index_js_ref_3_oneOf_1_0_node_modules_css_loader_dist_cjs_js_ref_3_oneOf_1_1_node_modules_vue_loader_lib_loaders_stylePostLoader_js_node_modules_postcss_loader_src_index_js_ref_3_oneOf_1_2_node_modules_vue_loader_lib_index_js_vue_loader_options_TrackComponent_vue_vue_type_style_index_0_id_4478e97e_scoped_true_lang_css___WEBPACK_IMPORTED_MODULE_0__);
+/* harmony reexport (unknown) */ for(var __WEBPACK_IMPORT_KEY__ in _node_modules_vuetify_loader_lib_loader_js_ref_4_node_modules_vue_style_loader_index_js_ref_3_oneOf_1_0_node_modules_css_loader_dist_cjs_js_ref_3_oneOf_1_1_node_modules_vue_loader_lib_loaders_stylePostLoader_js_node_modules_postcss_loader_src_index_js_ref_3_oneOf_1_2_node_modules_vue_loader_lib_index_js_vue_loader_options_TrackComponent_vue_vue_type_style_index_0_id_4478e97e_scoped_true_lang_css___WEBPACK_IMPORTED_MODULE_0__) if(["default"].indexOf(__WEBPACK_IMPORT_KEY__) < 0) (function(key) { __webpack_require__.d(__webpack_exports__, key, function() { return _node_modules_vuetify_loader_lib_loader_js_ref_4_node_modules_vue_style_loader_index_js_ref_3_oneOf_1_0_node_modules_css_loader_dist_cjs_js_ref_3_oneOf_1_1_node_modules_vue_loader_lib_loaders_stylePostLoader_js_node_modules_postcss_loader_src_index_js_ref_3_oneOf_1_2_node_modules_vue_loader_lib_index_js_vue_loader_options_TrackComponent_vue_vue_type_style_index_0_id_4478e97e_scoped_true_lang_css___WEBPACK_IMPORTED_MODULE_0__[key]; }) }(__WEBPACK_IMPORT_KEY__));
 
 
 /***/ }),
@@ -1047,7 +1047,7 @@ __webpack_require__.r(__webpack_exports__);
 var ___CSS_LOADER_API_IMPORT___ = __webpack_require__(5);
 var ___CSS_LOADER_EXPORT___ = ___CSS_LOADER_API_IMPORT___(false);
 // Module
-___CSS_LOADER_EXPORT___.push([module.i, ".track__wrapper[data-v-7b653b98]{display:flex;flex-direction:column;height:15%;border:1px solid grey;margin:5px;padding:0 10px;flex-basis:6rem;justify-content:center}.track__time[data-v-7b653b98]{padding:5px}.track__text[data-v-7b653b98]{display:flex;flex-direction:column;justify-content:center;padding:0 5px;margin:5px;border:1px solid #000}", ""]);
+___CSS_LOADER_EXPORT___.push([module.i, ".track__wrapper[data-v-4478e97e]{display:flex;flex-direction:column;height:15%;border:1px solid grey;margin:5px;padding:0 10px;flex-basis:6rem;justify-content:center}.track__time[data-v-4478e97e]{display:flex;padding:5px}.input__time[data-v-4478e97e]{width:200px}.track__text[data-v-4478e97e]{display:flex;flex-direction:column;justify-content:center;padding:0 5px;margin:5px;border:1px solid #000}", ""]);
 // Exports
 module.exports = ___CSS_LOADER_EXPORT___;
 
@@ -1122,23 +1122,28 @@ var staticRenderFns = []
 // EXTERNAL MODULE: ./components/VideoComponent.vue + 4 modules
 var VideoComponent = __webpack_require__(269);
 
-// CONCATENATED MODULE: ./node_modules/vuetify-loader/lib/loader.js??ref--4!./node_modules/vue-loader/lib/loaders/templateLoader.js??vue-loader-options!./node_modules/vue-loader/lib??vue-loader-options!./components/TrackComponent.vue?vue&type=template&id=7b653b98&scoped=true&
-var TrackComponentvue_type_template_id_7b653b98_scoped_true_render = function () {var _vm=this;var _h=_vm.$createElement;var _c=_vm._self._c||_h;return _c('div',{staticClass:"track__wrapper"},[_vm._ssrNode("<div class=\"track__time\" data-v-7b653b98><h3 data-v-7b653b98>"+_vm._ssrEscape(_vm._s(_vm.start)+" --> "+_vm._s(_vm.end))+"</h3></div> <div class=\"track__text\" data-v-7b653b98><p data-v-7b653b98>"+(_vm._ssrList((_vm.segmentText),function(c,index){return (_vm._ssrEscape("\n                "+_vm._s(c)+" ")+"<br data-v-7b653b98>")}))+"</p></div>")])}
-var TrackComponentvue_type_template_id_7b653b98_scoped_true_staticRenderFns = []
+// EXTERNAL MODULE: ./node_modules/vuetify/lib/components/VTextField/VTextField.js + 3 modules
+var VTextField = __webpack_require__(214);
+
+// CONCATENATED MODULE: ./node_modules/vuetify-loader/lib/loader.js??ref--4!./node_modules/vue-loader/lib/loaders/templateLoader.js??vue-loader-options!./node_modules/vue-loader/lib??vue-loader-options!./components/TrackComponent.vue?vue&type=template&id=4478e97e&scoped=true&
 
 
-// CONCATENATED MODULE: ./components/TrackComponent.vue?vue&type=template&id=7b653b98&scoped=true&
+var TrackComponentvue_type_template_id_4478e97e_scoped_true_render = function () {var _vm=this;var _h=_vm.$createElement;var _c=_vm._self._c||_h;return _c('div',{staticClass:"track__wrapper"},[_vm._ssrNode("<div class=\"track__time\" data-v-4478e97e>","</div>",[_c(VTextField["a" /* default */],{staticClass:"input__time",attrs:{"outlined":"","dense":"","hide-details":""},model:{value:(_vm.start),callback:function ($$v) {_vm.start=$$v},expression:"start"}}),_vm._ssrNode(" \n        --&gt; \n        "),_c('vt-text-field',{staticClass:"input__time",attrs:{"outlined":"","dense":""},model:{value:(_vm.end),callback:function ($$v) {_vm.end=$$v},expression:"end"}})],2),_vm._ssrNode(" <div class=\"track__text\" data-v-4478e97e><p data-v-4478e97e>"+(_vm._ssrList((_vm.segmentText),function(c,index){return (_vm._ssrEscape("\n                "+_vm._s(c)+" ")+"<br data-v-4478e97e>")}))+"</p></div>")],2)}
+var TrackComponentvue_type_template_id_4478e97e_scoped_true_staticRenderFns = []
+
+
+// CONCATENATED MODULE: ./components/TrackComponent.vue?vue&type=template&id=4478e97e&scoped=true&
 
 // CONCATENATED MODULE: ./node_modules/vuetify-loader/lib/loader.js??ref--4!./node_modules/babel-loader/lib??ref--2-0!./node_modules/vue-loader/lib??vue-loader-options!./components/TrackComponent.vue?vue&type=script&lang=js&
 /* harmony default export */ var TrackComponentvue_type_script_lang_js_ = ({
   props: {
     start: {
       type: String,
-      default: "00:00.000"
+      default: "00:00:00.000"
     },
     end: {
       type: String,
-      default: "00:00.000"
+      default: "00:00:00.000"
     },
     text: {
       type: String,
@@ -1167,15 +1172,17 @@ var TrackComponentvue_type_template_id_7b653b98_scoped_true_staticRenderFns = []
   methods: {
     msToString(time) {
       if (time === 0) {
-        return "00:00.000";
+        return "00:00:00.000";
       } else {
-        const min = time / 60000 >= 1 ? parseInt(time / 60000) : 0;
-        const sec = min >= 1 ? time % 60000 / 0.6 / 1000 >= 1 ? parseInt(time % 60000 / 0.6 / 1000) : 0 : time / 1000 >= 1 ? parseInt(time / 1000) : 0;
-        const mill = min >= 1 ? parseInt(time % 60000 / 0.6 % 100) : time / 1000 >= 1 ? time % 1000 : time;
+        const hour = time / 3600000 >= 1 ? parseInt(time / 3600000) : 0;
+        const min = time / 60000 >= 1 ? time / 60000 >= 60 ? parseInt(time / 60000) - 60 >= 1 ? parseInt(time / 60000) - 60 : 0 : parseInt(time / 60000) : 0;
+        const sec = time % 60000 >= 1 ? parseInt(time % 60000 / 1000) >= 1 ? parseInt(time % 60000 / 1000) : 0 : parseInt(time / 1000) >= 1 ? parseInt(time / 1000) : 0;
+        const mill = time % 1000;
+        const ooHour = hour / 10 >= 1 ? hour : `0${hour}`;
         const ooMin = min / 10 >= 1 ? min : `0${min}`;
         const ooSec = sec / 10 >= 1 ? sec : `0${sec}`;
         const oooMill = mill / 100 >= 1 ? mill : mill / 10 >= 1 ? `0${mill}` : `00${mill}`;
-        return `${ooMin} : ${ooSec}.${oooMill}`;
+        return `${ooHour}:${ooMin}:${ooSec}.${oooMill}`;
       }
     }
 
@@ -1201,11 +1208,11 @@ if (style0.__inject__) style0.__inject__(context)
 
 var component = Object(componentNormalizer["a" /* default */])(
   components_TrackComponentvue_type_script_lang_js_,
-  TrackComponentvue_type_template_id_7b653b98_scoped_true_render,
-  TrackComponentvue_type_template_id_7b653b98_scoped_true_staticRenderFns,
+  TrackComponentvue_type_template_id_4478e97e_scoped_true_render,
+  TrackComponentvue_type_template_id_4478e97e_scoped_true_staticRenderFns,
   false,
   injectStyles,
-  "7b653b98",
+  "4478e97e",
   "726ced74"
   
 )
--- components/TrackComponent.vue
+++ components/TrackComponent.vue
@@ -1,7 +1,20 @@
 <template>
     <div class="track__wrapper">
         <div class="track__time">
-            <h3>{{ start }} --> {{ end }}</h3>
+            <v-text-field
+                class="input__time"
+                v-model="start" 
+                outlined 
+                dense 
+                hide-details
+            /> 
+            --> 
+            <vt-text-field
+                class="input__time" 
+                v-model="end" 
+                outlined 
+                dense 
+            />
         </div>
         <div class="track__text">
             <p>
@@ -25,7 +38,11 @@
     justify-content: center;
 }
 .track__time {
+    display: flex;
     padding: 5px;
+}
+.input__time {
+    width: 200px;
 }
 .track__text {
     display: flex;
@@ -42,11 +59,11 @@ export default {
     props: {
         start: {
             type: String,
-            default: "00:00.000"
+            default: "00:00:00.000"
         },
         end: {
             type: String,
-            default: "00:00.000"
+            default: "00:00:00.000"
         },
         text: {
             type: String,
@@ -72,15 +89,17 @@ export default {
     methods: {
         msToString(time) {
             if (time === 0) {
-                return "00:00.000";
+                return "00:00:00.000";
             } else {
-                const min = (time / 60000) >= 1 ? parseInt(time/60000) : 0;
-                const sec = min >= 1 ? (time % 60000 / 0.6 / 1000) >= 1 ? parseInt((time % 60000) / 0.6 / 1000) : 0 : (time / 1000) >= 1 ? parseInt(time / 1000) : 0;
-                const mill = min >= 1 ? parseInt((time % 60000) / 0.6 % 100) : (time / 1000) >= 1 ? time % 1000 : time;
+                const hour = (time / 3600000) >= 1 ? parseInt(time/3600000) : 0;
+                const min = (time / 60000) >= 1 ? (time / 60000) >= 60 ? parseInt(time / 60000) - 60 >= 1 ? parseInt(time / 60000) - 60 : 0 : parseInt(time / 60000) : 0;
+                const sec = (time % 60000) >= 1 ? parseInt(time % 60000 / 1000) >= 1 ? parseInt(time % 60000 / 1000) : 0 : parseInt(time / 1000) >= 1 ? parseInt(time / 1000) : 0;
+                const mill = time % 1000;
+                const ooHour = (hour / 10) >= 1 ? hour : `0${hour}`;
                 const ooMin = (min / 10) >= 1 ? min : `0${min}`;
                 const ooSec = (sec / 10) >= 1 ? sec : `0${sec}`;
                 const oooMill = (mill / 100) >= 1 ? mill : (mill / 10) >= 1 ? `0${mill}` : `00${mill}`;
-                return `${ooMin} : ${ooSec}.${oooMill}`;
+                return `${ooHour}:${ooMin}:${ooSec}.${oooMill}`;
             }
         }
     }
