2137f75b64780489cdc35f2ba9d7975c84d22b18
hantae96 <hantae96@naver.com>
2022-07-21 14:28:12 +0900

    Update : 텍스트 페이지 레이아웃

--- .nuxt/App.js
+++ .nuxt/App.js
@@ -12,10 +12,10 @@ import _6f6c098b from '..\\layouts\\default.vue'
 import _f8703ec6 from '..\\layouts\\RTtrackLayout.vue'
 import _74887158 from '..\\layouts\\signupLayout.vue'
 import _14693a1c from '..\\layouts\\TestLayout.vue'
-import _f34131a6 from '..\\layouts\\textLayout.vue'
+import _059d7966 from '..\\layouts\\TextLayout.vue'
 import _0eb299af from '..\\layouts\\VideoLayout.vue'
 
-const layouts = { "_adminLayout": sanitizeComponent(_501d71a3),"_default": sanitizeComponent(_6f6c098b),"_RTtrackLayout": sanitizeComponent(_f8703ec6),"_signupLayout": sanitizeComponent(_74887158),"_TestLayout": sanitizeComponent(_14693a1c),"_textLayout": sanitizeComponent(_f34131a6),"_VideoLayout": sanitizeComponent(_0eb299af) }
+const layouts = { "_adminLayout": sanitizeComponent(_501d71a3),"_default": sanitizeComponent(_6f6c098b),"_RTtrackLayout": sanitizeComponent(_f8703ec6),"_signupLayout": sanitizeComponent(_74887158),"_TestLayout": sanitizeComponent(_14693a1c),"_TextLayout": sanitizeComponent(_059d7966),"_VideoLayout": sanitizeComponent(_0eb299af) }
 
 export default {
   render (h, props) {
